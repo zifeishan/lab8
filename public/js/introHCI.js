0347fa808b4d5294c9a519161ad553877ddbbdc8
Zifei Shan <shanzifei@pku.edu.cn>
2014-02-28 23:08:55 -0800

    Basic requirements

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -10,4 +10,8 @@ $(document).ready(function() {
  */
 function initializePage() {
 	// your code here
+  $('.likeBtn').click(function(e){
+    // ga("send", "event", "Category", "Action");
+    ga("send", "event", "like", "click");
+  })
 }
